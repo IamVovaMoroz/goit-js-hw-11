2beba199e4af9d2fc46202ec396a8e54def3ca10
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-04-07 13:19:53 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 // Получаем доступ к форме
 const searchFormRef = document.querySelector('#search-form');
 const loadMoreBtnRef = document.querySelector('.load-more');
-const galleryRef = document.querySelector('.gallery');
+export const galleryRef = document.querySelector('.gallery');
 
 let inputValue = '';
 let lastInputValue = '';
@@ -130,32 +130,4 @@ async function addImages () {
   } catch (error) {
     console.log(error.message);
   }
-}
-
-function markupImage (arrayOfResults) {
-  markup = arrayOfResults
-    .map(
-      ({ largeImageURL, webformatURL, likes, views, comments, downloads }) =>
-        `<div class="photo-card">
-                <a href="${largeImageURL}">
-                <div class="thumb">
-                <img 
-                  src="${webformatURL}"
-                  alt=" ${webformatURL}"
-                  loading="lazy"
-                  />
-                  </div>
-                </a>
-                <div class="info">
-                  <p class="info-item"><b>Likes</b><br> ${likes}</p>
-                  <p class="info-item"><b>Views</b><br> ${views}</p>
-                  <p class="info-item"><b>Comments</b><br> ${comments}</p>
-                  <p class="info-item"><b>Downloads</b><br> ${downloads}</p>
-                </div>
-              </div>`
-    )
-    .join('');
-
-  galleryRef.insertAdjacentHTML('beforeend', markup);
-  lightbox.refresh();
 }
