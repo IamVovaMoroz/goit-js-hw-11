318f25452b4f664aeec0f0d7d6d43968650bb5e5
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-04-07 13:48:48 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -131,30 +131,30 @@ async function addImages () {
   }
 }
 
-// function markupImage (arrayOfResults) {
-//   markup = arrayOfResults
-//     .map(
-//       ({ largeImageURL, webformatURL, likes, views, comments, downloads }) =>
-//         `<div class="photo-card">
-//                     <a href="${largeImageURL}">
-//                     <div class="thumb">
-//                     <img
-//                       src="${webformatURL}"
-//                       alt=" ${webformatURL}"
-//                       loading="lazy"
-//                       />
-//                       </div>
-//                     </a>
-//                     <div class="info">
-//                       <p class="info-item"><b>Likes</b><br> ${likes}</p>
-//                       <p class="info-item"><b>Views</b><br> ${views}</p>
-//                       <p class="info-item"><b>Comments</b><br> ${comments}</p>
-//                       <p class="info-item"><b>Downloads</b><br> ${downloads}</p>
-//                     </div>
-//                   </div>`
-//     )
-//     .join('');
+function markupImage (arrayOfResults) {
+  markup = arrayOfResults
+    .map(
+      ({ largeImageURL, webformatURL, likes, views, comments, downloads }) =>
+        `<div class="photo-card">
+                    <a href="${largeImageURL}">
+                    <div class="thumb">
+                    <img
+                      src="${webformatURL}"
+                      alt=" ${webformatURL}"
+                      loading="lazy"
+                      />
+                      </div>
+                    </a>
+                    <div class="info">
+                      <p class="info-item"><b>Likes</b><br> ${likes}</p>
+                      <p class="info-item"><b>Views</b><br> ${views}</p>
+                      <p class="info-item"><b>Comments</b><br> ${comments}</p>
+                      <p class="info-item"><b>Downloads</b><br> ${downloads}</p>
+                    </div>
+                  </div>`
+    )
+    .join('');
 
-//   galleryRef.insertAdjacentHTML('beforeend', markup);
-//   lightbox.refresh();
-// }
+  galleryRef.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
