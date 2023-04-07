b45abf54c94d252b3392b7bc7cd22387355985ad
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-04-07 13:33:46 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import debounce from 'lodash.debounce';
 import './sass/_common.scss';
-// import { markupImage } from '..src/';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -131,3 +130,31 @@ async function addImages () {
     console.log(error.message);
   }
 }
+
+// function markupImage (arrayOfResults) {
+//   markup = arrayOfResults
+//     .map(
+//       ({ largeImageURL, webformatURL, likes, views, comments, downloads }) =>
+//         `<div class="photo-card">
+//                     <a href="${largeImageURL}">
+//                     <div class="thumb">
+//                     <img
+//                       src="${webformatURL}"
+//                       alt=" ${webformatURL}"
+//                       loading="lazy"
+//                       />
+//                       </div>
+//                     </a>
+//                     <div class="info">
+//                       <p class="info-item"><b>Likes</b><br> ${likes}</p>
+//                       <p class="info-item"><b>Views</b><br> ${views}</p>
+//                       <p class="info-item"><b>Comments</b><br> ${comments}</p>
+//                       <p class="info-item"><b>Downloads</b><br> ${downloads}</p>
+//                     </div>
+//                   </div>`
+//     )
+//     .join('');
+
+//   galleryRef.insertAdjacentHTML('beforeend', markup);
+//   lightbox.refresh();
+// }
